eee634b4cc74af5333b62a8d67935c5625c1202f
astercasc <astercass@qq.com>
2023-08-09 09:56:57 +0800

    feat: add touka exception

---
+++ aster-yuno-index-object/src/main/java/com/aster/yuno/index/exception/CaskToukaException.java
@@ -0,0 +1,9 @@
+package com.aster.yuno.index.exception;
+
+public class CaskToukaException extends CaskRuntimeException {
+
+    public CaskToukaException(String message) {
+        super(message);
+    }
+
+}
