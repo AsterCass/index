f5dba4bec84e130aa82d7c561111afcf34eb6ae0
astercasc <astercass@qq.com>
2023-12-19 12:18:50 +0800

    feat: support nanana

---
+++ aster-yuno-index-object/src/main/java/com/aster/yuno/index/exception/CaskNananaException.java
@@ -0,0 +1,9 @@
+package com.aster.yuno.index.exception;
+
+public class CaskNananaException extends CaskRuntimeException {
+
+    public CaskNananaException(String message) {
+        super(message);
+    }
+
+}
